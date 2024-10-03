68a81bf2f44c91ec82581307fd93400251c2b134
Antea Runjić <132346858+antearunjic@users.noreply.github.com>
2024-10-03 09:33:47 +0200

    Update new-user.cy.js

--- new-user.cy.js
+++ new-user.cy.js
@@ -2,27 +2,11 @@ describe("template spec", () => {
   it("passes", () => {
     cy.visit("https://demoqa.com/register");
 
-    //Login successful
+    //Create new user
     cy.get("#firstname").type("antea");
     cy.get("#lastname").type("runjic");
     cy.get("#userName").type("antearunjic");
     cy.get("#password").type("AbCd12345@");
-
-    cy.get('iframe[src*="recaptcha"]', { timeout: 10000 })
-      .should("be.visible")
-      .then(($iframe) => {
-        // Access the iframe's content
-        const iframeBody = $iframe.contents().find("body");
-
-        // Attempt to click the checkbox
-        cy.wrap(iframeBody)
-          .find('span[role="checkbox"]')
-          .click({ force: true }); // Use force: true if the element is not interactable
-      });
-
-    // Optionally, verify that the checkbox is now checked
-    cy.get('span[role="checkbox"][aria-checked="true"]').should("exist");
-
     cy.get("#register").click();
     cy.url().should("include", "/profile");
   });
